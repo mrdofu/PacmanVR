5d447eb8a2610928cdcbefd4536474cc9fa02a4a
Chris Huynh <Chris Huynh>
2016-06-26 02:08:54 -0400

    ctionless map

---
+++ ENGHACK/Assets/MapPhysics.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapPhysics : MonoBehaviour {
+
+    private PhysicMaterial mapPhysics;
+	void Start () {
+        mapPhysics = GetComponent<PhysicMaterial>();
+        mapPhysics.dynamicFriction = 0;
+        mapPhysics.staticFriction = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
