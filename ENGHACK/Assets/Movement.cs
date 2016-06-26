690cdce1db9f2de51a3a32b137e406b553275d5a
mrdofu <intense_smurf@live.com>
2016-06-26 00:47:13 -0400

    added player movement based on camera angle

---
+++ ENGHACK/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float velocity;
+    public GameObject mainCam;
+    private Rigidbody rb;
+     
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vel_x = mainCam.transform.forward.x;
+        rb.velocity = new Vector3(vel_x, mainCam.transform.forward.y, mainCam.transform.forward.z);
+	}
+}
