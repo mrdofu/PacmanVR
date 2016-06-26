4b02fe2b59dbdb5c1a7a5ff1827860fb62903d9a
Chris Huynh <Chris Huynh>
2016-06-26 01:39:43 -0400

    Implemented variable velocity

---
+++ ENGHACK/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    GameObject player;
+    Transform spawnpoint;
+
+    private float speed = 0.0005f;
+    private Vector3 movement;
+    private int floorMask;
+    private CharacterController playerCC;
+    private Rigidbody playerRB;
+    private bool stopRequested = (Input.touchCount % 2) == 1;
+
+	// Use this for initialization
+	void Start () {
+        floorMask = LayerMask.GetMask("Floor");
+        playerRB = GetComponent<Rigidbody>();
+        playerCC = GetComponent<CharacterController>();
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (!stopRequested)
+        {
+            
+            Walk();
+        }
+    }
+
+    void Walk() {
+        playerRB.velocity = new Vector3();
+    }
+}
