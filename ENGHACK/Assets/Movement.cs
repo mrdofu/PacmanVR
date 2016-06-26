6d8730117578ee607d8f244f82d3f9c09359d3ba
Chris Huynh <Chris Huynh>
2016-06-26 10:49:12 -0400

    Retain inside map, PacMan movement

--- ENGHACK/Assets/Movement.cs
+++ ENGHACK/Assets/Movement.cs
@@ -7,19 +7,38 @@ public class Movement : MonoBehaviour {
     public GameObject mainCam;
     private Rigidbody rb;
     private float[] vel = new float[3]; //[0] = x, [1] = y, [2] = z
-     
-
+    
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         rb = gameObject.GetComponent<Rigidbody>();
         oldVelocity = velocity;
 	}
 
+    
+    bool requestStop()
+    {
+        //Press and hold to request stop
+        return Input.touchCount != 0;
+        
+    }
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if (requestStop())
+        {
+            velocity = 0;
+        }
+        else
+        {
+            velocity = oldVelocity;
+        }
+
         vel[0] = mainCam.transform.forward.x * velocity;
         vel[1] = mainCam.transform.forward.y * velocity;
         vel[2] = mainCam.transform.forward.z * velocity;
+        
 
         rb.velocity = new Vector3(vel[0], vel[1], vel[2]);
 	}
---
+++ ENGHACK/Assets/PacMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PacMovement : MonoBehaviour {
+    public float speed = 1f;
+    private Rigidbody rb;
+    private float[] vel = new float[3];
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vel[0] = gameObject.transform.forward.x * speed;
+        vel[1] = gameObject.transform.forward.y * speed;
+        vel[2] = gameObject.transform.forward.z * speed;
+
+        rb.velocity = new Vector3(vel[0], vel[1], vel[2]);
+    }
+}
