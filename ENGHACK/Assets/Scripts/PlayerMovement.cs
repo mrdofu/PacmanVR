19608b66c63a6db64897d9965b34e8d11119087e
mrdofu <intense_smurf@live.com>
2016-09-18 17:12:06 -0400

    updated gvr

---
+++ ENGHACK/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float velocity = 1f;
+    private float oldVelocity;
+    public GameObject mainCam;
+    private Rigidbody rb;
+    private float[] vel = new float[3]; //[0] = x, [1] = y, [2] = z
+    
+	// Use this for initialization
+	void Start ()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        oldVelocity = velocity;
+	}
+
+    
+    bool requestStop()
+    {
+        //Press and hold to request stop
+        return Input.touchCount != 0;
+        
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (requestStop())
+        {
+            velocity = 0;
+        }
+        else
+        {
+            velocity = oldVelocity;
+        }
+
+        vel[0] = mainCam.transform.forward.x * velocity;
+        vel[1] = mainCam.transform.forward.y * velocity;
+        vel[2] = mainCam.transform.forward.z * velocity;
+        
+
+        rb.velocity = new Vector3(vel[0], vel[1], vel[2]);
+	}
+}
