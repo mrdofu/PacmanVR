0a32253bcf191a6c07a213401e09018efe11543c
mrdofu <intense_smurf@live.com>
2017-03-23 20:26:26 -0400

    added nav mesh agent to pacman and edited so he's moving

---
+++ ENGHACK/Assets/Scripts/Pac/PacMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacMovement : MonoBehaviour {
+
+    [SerializeField]
+    NavMeshAgent navAgent;
+    public Transform t;
+
+	// Use this for initialization
+	void Start () {
+        navAgent.SetDestination(t.position);
+        navAgent.Stop();
+	}
+	
+    void OnEnable()
+    {
+        GameManager.OnGameStateChanged += GameManager_onGameStateChanged;
+    }
+
+    /*
+     * callback for gamemanager pause
+     */
+    private void GameManager_onGameStateChanged(bool isPaused)
+    {
+        if (isPaused)
+        {
+            navAgent.Stop();
+        } else
+        {
+            navAgent.Resume();
+        }
+    }
+}
--- ENGHACK/Assets/Scripts/Pac/Pacman.cs
+++ ENGHACK/Assets/Scripts/Pac/Pacman.cs
@@ -2,46 +2,19 @@
 
 public class Pacman : MonoBehaviour {
     public int numLives = 3;
-    private static float MOVE_SPEED = 1f;
-    public float currentSpeed;
-
+    
     private Rigidbody rb;
-    private float[] vel = new float[3];
-
 
     // Use this for initialization
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        currentSpeed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        updateVelocity();
+
     }
 
-    void OnEnable()
-    {
-        GameManager.OnGameStateChanged += GameManager_onGameStateChanged;
-    }
-
-    /*
-     * callback for gamemanager pause
-     */
-    private void GameManager_onGameStateChanged(bool isPaused)
-    {
-        currentSpeed = isPaused ? 0 : MOVE_SPEED;
-    }
-
-    void updateVelocity()
-    {
-        // the transform is rotated or something
-        vel[0] = gameObject.transform.forward.z * currentSpeed;
-        vel[1] = gameObject.transform.forward.y * currentSpeed;
-        vel[2] = gameObject.transform.forward.x * currentSpeed;
-
-        rb.velocity = new Vector3(vel[0], vel[1], vel[2]);
-    }
 }
