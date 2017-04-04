63ae846229ed134db85731a081a0de32f028f823
mrdofu <intense_smurf@live.com>
2017-04-04 18:44:49 -0400

    ghosts now greedily hunt pacman

---
+++ ENGHACK/Assets/Scripts/Ghost/GhostMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    NavMeshAgent navAgent;
+    GameObject pacGo;
+    Transform goal;
+
+    // Use this for initialization
+    void Start()
+    {
+        pacGo = GameObject.Find("PACMAN");
+        goal = pacGo.transform;
+        navAgent.SetDestination(goal.position);
+        navAgent.Stop();
+    }
+
+    void Update()
+    {
+        goal = pacGo.transform;
+        navAgent.SetDestination(goal.position);
+    }
+
+    void OnEnable()
+    {
+        GameManager.OnGameStateChanged += GameManager_onGameStateChanged;
+    }
+
+    /*
+     * callback for gamemanager pause
+     */
+    private void GameManager_onGameStateChanged(bool isPaused)
+    {
+        if (isPaused)
+        {
+            navAgent.Stop();
+        }
+        else
+        {
+            navAgent.Resume();
+        }
+    }
+}
