63370c3ea934173970c38e4182732d29ac9ec59b
mrdofu <intense_smurf@live.com>
2017-05-11 23:14:43 -0400

    refactored public fields into properties, made Cell compatible with priority queue class

--- ENGHACK/Assets/Scripts/Movement/Cell.cs
+++ ENGHACK/Assets/Scripts/Movement/Cell.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
+using System;
 
 /**
- * Grid cell for navigation
+ * Grid cell representation of map for navigation
  */
-public class Cell
+public class Cell: IComparable<Cell>
 {
-    public bool walkable;
-    public Vector3 worldPosition;
-    public int gridX;
-    public int gridY;
+    public bool walkable { get; private set; }
+    public Vector3 worldPosition { get; private set; }
+    public int gridX { get; private set; }
+    public int gridY { get; private set; }
 
-    public int gCost;
-    public Cell parent;
+    private int _cost = -1;
+    public int cost {
+        get
+        {
+            return _cost;
+        }
+        set
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException("Cost must be greater than or equal to 0.");
+            _cost = value;
+        }
+    }
 
     public Cell(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
     {
@@ -21,4 +32,12 @@ public class Cell
         gridY = _gridY;
     }
 
+    public int CompareTo(Cell other)
+    {
+        int comp = cost - other.cost;
+        if (comp < 0) comp = -1;
+        else if (comp > 0) comp = 1;
+
+        return comp;
+    }
 }
