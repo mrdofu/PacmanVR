d93ffc3f014f703d4b7d7055a34401cd17e1d9c1
mrdofu <intense_smurf@live.com>
2017-06-15 18:20:55 -0400

    refactored ghost adding costs to cells into its own script

---
+++ ENGHACK/Assets/Scripts/Ghost/GhostGridInteraction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GhostGridInteraction : MonoBehaviour {
+
+    Grid mapGrid;
+
+    void Start () {
+        mapGrid = GameObject.Find("map").GetComponent<Grid>();
+    }
+
+    void Update () {
+        addCellCosts();
+    }
+
+    /**
+     * Adds cell costs to cells surrounding ghost for pacman's pathfinding
+     */
+    private void addCellCosts()
+    {
+        // reset the cells in a greater area than a ghost's cost area so that cells the ghost leaves are reset
+        Cell ghostCell = mapGrid.CellFromWorldPoint(transform.position);
+        // TODO: reset
+        // add costs
+        IDictionary<Cell, bool> visited = new Dictionary<Cell, bool>();
+        visited.Add(ghostCell, true);
+        Queue<Cell> toVisit = new Queue<Cell>(mapGrid.GetCellNeighbours(ghostCell));
+        for (int dist = 1, range = 4; dist < range; dist++)
+        {
+            int cellCost = (range - dist) * (range - dist);
+            for (int i = 0, size = toVisit.Count; i < size; i++)
+            {
+                Cell current = toVisit.Dequeue();
+                current.cost = cellCost;
+                visited.Add(current, true);
+                List<Cell> nextDistNeighbs = mapGrid.GetCellNeighbours(current);
+                foreach (Cell c in nextDistNeighbs)
+                {
+                    if (!visited.ContainsKey(c))
+                    {
+                        toVisit.Enqueue(c);
+                    }
+                }
+            }
+        }
+    }
+}
--- ENGHACK/Assets/Scripts/Ghost/GhostMovement.cs
+++ ENGHACK/Assets/Scripts/Ghost/GhostMovement.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 
 public class GhostMovement : MonoBehaviour
 {
@@ -9,8 +8,6 @@ public class GhostMovement : MonoBehaviour
     GameObject pacGo;
     Transform goal;
 
-    Grid mapGrid;
-
     // Use this for initialization
     void Start()
     {
@@ -18,52 +15,17 @@ public class GhostMovement : MonoBehaviour
         goal = pacGo.transform;
         navAgent.SetDestination(goal.position);
         navAgent.Stop();
-
-        mapGrid = GameObject.Find("map").GetComponent<Grid>();
     }
 
     void Update()
     {
         goal = pacGo.transform;
         navAgent.SetDestination(goal.position);
-        addCellCosts();
     }
 
     void OnEnable()
     {
         GameManager.OnGameStateChanged += GameManager_onGameStateChanged;
-    }
-
-    /**
-     * Adds cell costs to cells surrounding ghost for pacman's pathfinding
-     */
-    private void addCellCosts()
-    {
-        // reset the cells in a greater area than a ghost's cost area so that cells the ghost leaves are reset
-        Cell ghostCell = mapGrid.CellFromWorldPoint(transform.position);
-        // reset
-        // add costs
-        IDictionary<Cell, bool> visited = new Dictionary<Cell, bool>();
-        visited.Add(ghostCell, true);
-        Queue<Cell> toVisit = new Queue<Cell>(mapGrid.GetCellNeighbours(ghostCell));
-        for (int dist = 1, range = 4; dist < range; dist++)
-        {
-            int cellCost = (range - dist) * (range - dist);
-            for (int i = 0, size = toVisit.Count; i < size; i++)
-            {
-                Cell current = toVisit.Dequeue();
-                current.cost = cellCost;
-                visited.Add(current, true);
-                List<Cell> nextDistNeighbs = mapGrid.GetCellNeighbours(current);
-                foreach (Cell c in nextDistNeighbs)
-                {
-                    if (!visited.ContainsKey(c))
-                    {
-                        toVisit.Enqueue(c);
-                    }
-                }
-            }
-        }
     }
 
     /*
