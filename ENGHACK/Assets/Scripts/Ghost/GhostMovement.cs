2e86c3e5e65adfb89223ea2c6fb443d10f9dfea4
mrdofu <intense_smurf@live.com>
2017-06-15 00:12:08 -0400

    added groundwork to add costs to cells

--- ENGHACK/Assets/Scripts/Ghost/GhostMovement.cs
+++ ENGHACK/Assets/Scripts/Ghost/GhostMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class GhostMovement : MonoBehaviour
 {
@@ -17,17 +18,52 @@ public class GhostMovement : MonoBehaviour
         goal = pacGo.transform;
         navAgent.SetDestination(goal.position);
         navAgent.Stop();
+
+        mapGrid = GameObject.Find("map").GetComponent<Grid>();
     }
 
     void Update()
     {
         goal = pacGo.transform;
         navAgent.SetDestination(goal.position);
+        addCellCosts();
     }
 
     void OnEnable()
     {
         GameManager.OnGameStateChanged += GameManager_onGameStateChanged;
+    }
+
+    /**
+     * Adds cell costs to cells surrounding ghost for pacman's pathfinding
+     */
+    private void addCellCosts()
+    {
+        // reset the cells in a greater area than a ghost's cost area so that cells the ghost leaves are reset
+        Cell ghostCell = mapGrid.CellFromWorldPoint(transform.position);
+        // reset
+        // add costs
+        IDictionary<Cell, bool> visited = new Dictionary<Cell, bool>();
+        visited.Add(ghostCell, true);
+        Queue<Cell> toVisit = new Queue<Cell>(mapGrid.GetCellNeighbours(ghostCell));
+        for (int dist = 1, range = 4; dist < range; dist++)
+        {
+            int cellCost = (range - dist) * (range - dist);
+            for (int i = 0, size = toVisit.Count; i < size; i++)
+            {
+                Cell current = toVisit.Dequeue();
+                current.cost = cellCost;
+                visited.Add(current, true);
+                List<Cell> nextDistNeighbs = mapGrid.GetCellNeighbours(current);
+                foreach (Cell c in nextDistNeighbs)
+                {
+                    if (!visited.ContainsKey(c))
+                    {
+                        toVisit.Enqueue(c);
+                    }
+                }
+            }
+        }
     }
 
     /*
