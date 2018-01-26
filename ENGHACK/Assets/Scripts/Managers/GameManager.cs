43ef2b3cc09b33473c6d4cecca968aab1274c29e
mrdofu <intense_smurf@live.com>
2018-01-26 00:03:37 -0500

    added timer to GameManager

--- ENGHACK/Assets/Scripts/Managers/GameManager.cs
+++ ENGHACK/Assets/Scripts/Managers/GameManager.cs
@@ -2,8 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     public const int WINNER_PACMAN = 0;     // if pacman eats all dots before time runs out
     public const int WINNER_GHOST = 1;      // if pacman loses all lives or time runs out
     
@@ -13,46 +12,53 @@ public class GameManager : MonoBehaviour
     public static event Action OnGameReset;     // soft reset (ie pacman dies)
     public static event Action OnGameRestart;   // hard game reset (ie after game over)
 
-    void Start()
-    {
+    public enum GameState {
+        Pause, Play
+    };
+    public static GameState CurrentState { get; private set; }
+
+    const int MATCH_LENGTH = 3 * 60; // 3 minutes, in seconds
+    private GameTimer matchTimer;
+
+    void Start() {
         OnGamePaused();
+        matchTimer = new GameTimer(MATCH_LENGTH);
     }
 
-    void OnEnable()
-    {
+    void OnEnable() {
         PlayScaleTarget.OnPlayButtonComplete += PlayScaleTarget_OnPlayButtonComplete;
         Pacman.OnLoseLife += Pacman_OnLoseLife;
         Pacman.OnLoseAllLives += Pacman_OnLoseAllLives;
         Dot.OnAllDotsEaten += Dot_OnAllDotsEaten;
         OnGameRestart += GameManager_OnGameRestart;
+        OnGamePaused += GameManager_OnGamePaused;
+        OnGamePlayed += GameManager_OnGamePlayed;
+        matchTimer.OnTimerComplete += MatchTimer_OnTimerComplete;
     }
 
-    void OnDisable()
-    {
+    void OnDisable() {
         PlayScaleTarget.OnPlayButtonComplete -= PlayScaleTarget_OnPlayButtonComplete;
         Pacman.OnLoseLife -= Pacman_OnLoseLife;
         Pacman.OnLoseAllLives -= Pacman_OnLoseAllLives;
         Dot.OnAllDotsEaten -= Dot_OnAllDotsEaten;
         OnGameRestart -= GameManager_OnGameRestart;
+        OnGamePaused -= GameManager_OnGamePaused;
+        OnGamePlayed -= GameManager_OnGamePlayed;
+        matchTimer.OnTimerComplete -= MatchTimer_OnTimerComplete;
     }
 
     /**
      * To be called after all pacman lives are lost or after time runs out or all dots are consumed
      * @param winner 0 if pacman wins, 1 if ghosts win
      */
-    void GameOver(int winner)
-    {
+    void GameOver(int winner) {
         // Determine winner based on parameter
         GameObject[] hudArray = GameObject.FindGameObjectsWithTag("HUD");
-        foreach (var hudObj in hudArray)
-        {
+        foreach (var hudObj in hudArray) {
             Text gameStatusText = hudObj.transform.Find("gameStatusText").GetComponent<Text>();
-            if (winner == 0)
-            {
+            if (winner == 0) {
                 gameStatusText.text = "PACMAN WINS";
-            }
-            else
-            {
+            } else {
                 gameStatusText.text = "GHOSTS WIN";
             }
         }
@@ -78,6 +84,20 @@ public class GameManager : MonoBehaviour
 
     private void GameManager_OnGameRestart() {
         OnGamePaused();
+        matchTimer.ResetTimer();
+    }
+
+    private void GameManager_OnGamePaused() {
+        CurrentState = GameState.Pause;
+    }
+
+    private void GameManager_OnGamePlayed() {
+        CurrentState = GameState.Play;
+    }
+
+    private void MatchTimer_OnTimerComplete() {
+        // pacman loses since he hasn't eaten all the dots by now
+        GameOver(WINNER_GHOST);
     }
 
     // needs to be public since event module on ui element calls this (not C# event)
---
+++ ENGHACK/Assets/Scripts/Utils/GameTimer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+/**
+ * In-game timer that follows rules of pause, play, etc
+ */
+public class GameTimer : MonoBehaviour {
+
+    public event Action OnTimerComplete;
+
+    public float TimeLeft { get; private set; }
+
+    private float timerLength;
+    private bool timerActive = false;
+
+    public GameTimer(float length) {
+        timerLength = length;
+    }
+
+    void Update() {
+        // can be paused, in which case, don't count time
+        if (GameManager.CurrentState == GameManager.GameState.Play && timerActive) {
+            if (TimeLeft > 0) {
+                TimeLeft -= Time.deltaTime;
+            } else {
+                timerActive = false;
+                OnTimerComplete();
+            }
+        }
+    }
+
+    public void ResetTimer() {
+        TimeLeft = timerLength;
+        timerActive = true;
+    }
+}
